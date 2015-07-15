40b6607cf6ef983a273fd44c9dbd10e586f646d9
Dikshith <dikshith.sh@gmail.com>
2015-07-15 05:21:41 +0200

    Initial implementation

---
+++ MongeElkan.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 15 04:59:06 2015
+
+@author: dikshith
+
+Implementation of Monge-Eklan algorithm.
+Using Jaro-winkler as inner similarity function. 
+you can use levenshtein or jaro distance as well.
+"""
+
+import jellyfish as j
+
+class MongeEklan():
+    '''
+    Accepts the inner similarity function
+    '''
+    def __init__(self):
+        pass
+    
+    def score(self,s,t):
+        '''
+        Input: s - multi-word string
+               t - multi-word string
+        Output : score
+        Note: In single word string, score = jaro-winkler score
+        '''
+        cummax = 0
+        maxscore=0
+        for ws in s.split(" "):
+            for wt in t.split(" "):
+                maxscore = max(maxscore,j.jaro_winkler(ws,wt))
+            cummax += maxscore
+        
+        return cummax/len(s.split(" "))
+        
+def main():
+    m = MongeEklan()
+    print(m.score("peter christen","christian pedro"))
+    
+if __name__ == '__main__':
+    main()
